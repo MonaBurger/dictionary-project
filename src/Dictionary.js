ff95f0d954f6d9f734c005084c4720261a9b57b3
MonaBurger <mona@software-lab.de>
2024-03-26 23:42:52 +0100

    added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    let apiKey = "484c5880942e8bc04a5205c53o4et26f";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
